28a475fd8d8df7bdd26ea362fc14ec91fa0b4267
Nikola Ubavic <53820106+ubavic@users.noreply.github.com>
2021-10-27 23:23:05 +0200

    Implementiran raytracer
    Za svaki piksel u slici generiše se zrak koj počinje u koordinatnom
    početku. Funkcija 'presekZrakaISfere' proverava da li takav zrak seče
    sferu koja je predstavljena strukturom 'sfera1'.

--- zrak.py
+++ zrak.py
@@ -5,6 +5,11 @@ import math
 sirina = 1000
 visina = 600
 
+sfera1 = {
+    'centar': np.array([0, 0, -4]),
+    'pprecnik': 1
+}
+
 
 def duzina(vektor):
     return math.sqrt(np.dot(vektor, vektor))
@@ -19,10 +24,32 @@ def normiraj(vektor):
         return vektor/d
 
 
+def presekZrakaISfere(zrak, sfera):
+    ss = np.dot(sfera['centar'], sfera['centar'])
+    sz = np.dot(sfera['centar'], zrak['pravac'])
+
+    if sz > 0 and ss - sz * sz < sfera['pprecnik']:
+        return 1
+
+    return 0
+
+
+formatSlike = sirina/visina
 slika = np.zeros((visina, sirina, 3))
 
 for i in range(visina):
+    y = (1 - 2 * i/visina) / formatSlike
+
     for j in range(sirina):
-        slika[i, j] = np.array([0, i/visina, j/sirina])
+        x = -1 + 2 * j/sirina
+        zrak = {
+            'pravac': normiraj(np.array([x, y, -1])),
+            'tacka': np.array([0, 0, 0])
+        }
+
+        if presekZrakaISfere(zrak, sfera1):
+            slika[i, j] = np.array([1, 1, 1])
+        else:
+            slika[i, j] = np.array([0, 0, 0])
 
 matplotlib.pyplot.imsave('slika.png', slika)
